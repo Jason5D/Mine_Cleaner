9ce134d5c438be7191abb5b4229fcc507c938f35
Jason <jason5donoghue@gmail.com>
2023-07-14 13:13:07 +0100

    added dev branch

--- mine-cleaner/src/App.js
+++ mine-cleaner/src/App.js
@@ -1,9 +1,10 @@
-import Game from './Game.js';
-import './styles.css';
+import Game from "./Game.js";
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
+      <h1>Soon to Be Mine Cleaner</h1>
       <Game />
     </div>
   );
