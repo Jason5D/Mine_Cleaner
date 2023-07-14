546b1356dfa2b8e6fbdc8c98d20f855fea0f1868
Jason <jason5donoghue@gmail.com>
2023-07-14 13:12:27 +0100

    added basic boiler plate

---
+++ mine-cleaner/src/App.js
@@ -0,0 +1,12 @@
+import Game from './Game.js';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ mine-cleaner/src/Square.js
@@ -0,0 +1,8 @@
+// square component
+export default function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
